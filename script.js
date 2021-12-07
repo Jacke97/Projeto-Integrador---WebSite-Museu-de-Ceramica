e13e222562a1ae70908a6c02bde232e82f053119
Jacke97 <jafsilva97@hotmail.com>
2021-12-06 21:12:29 -0300

    site do museu

---
+++ script.js
@@ -0,0 +1,5 @@
+//função para que o botão do menu hamburguer seja exibido e oculto
+document.querySelector(".hamburguer").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+
+)
